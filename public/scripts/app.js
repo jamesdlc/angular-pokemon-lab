44f33e34b3612a0bc1117ccd66fca8b7bc050a97
jamestothe <jamesdelacruz@gmail.com>
2016-07-30 00:27:05 -0700

    delete button working but conlogging indexof -1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,20 +23,28 @@ function PokemonAppController($http) {
     }, function errorCallback(response) {
         console.log('There was an error getting the data', response);
     });
-    vm.deletePoke = function(poke) {
-
+    vm.deletePoke = function(poke) { //come back to this later, page doesn't render SAP
         $http({
             method: 'DELETE',
             url: 'https://super-crud.herokuapp.com/pokemon/' + poke
         }).then(function successCallback(json) {
-            // var index = vm.albums.indexOf(album);
-            // vm.albums.splice(index, 1);
+          console.log(json);
         }, function errorCallback(response) {
           console.log(poke);
             console.log('There was an error deleting the data', response);
         });
     };
 
+    vm.editPoke = function(poke) {
+      $http({
+        method: 'PUT',
+        url: 'https://super-crud.herokuapp.com/pokemon/' + poke
+      }).then(function successCallback(json) {
+        console.log(json);
+      }, function errorCallback(response) {
+        console.log('There was an error deleting the data', response);
+      });
+    };
 //     vm.createAlbum = function() {
 //         $http({
 //             method: 'POST',
@@ -48,26 +56,6 @@ function PokemonAppController($http) {
 //             console.log('There was an error posting the data', response);
 //         });
 //     };
-//     vm.deleteAlbum = function(album) {
-//         $http({
-//             method: 'DELETE',
-//             url: '/api/albums/' + album._id
-//         }).then(function successCallback(json) {
-//             var index = vm.albums.indexOf(album);
-//             vm.albums.splice(index, 1);
-//         }, function errorCallback(response) {
-//             console.log('There was an error deleting the data', response);
-//         });
-//     };
-//     vm.editAlbum = function(album) {
-//       $http({
-//           method: 'PUT',
-//           url: '/api/albums/' + album._id,
-//           data: album
-//       }).then(function successCallback(json) {
-//         console.log(json);
-//       }, function errorCallback(response) {
-//           console.log('There was an error deleting the data', response);
-//       });
+//
 //     };
 }
