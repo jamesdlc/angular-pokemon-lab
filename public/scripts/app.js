7de48a49ca22b1a87afbf7e997fbae9095acf048
jamestothe <jamesdelacruz@gmail.com>
2016-07-30 16:13:39 -0700

    PUT wont updated pokedex no or evolves from

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,50 +12,58 @@ PokemonAppController.$inject = ['$http'];
 
 function PokemonAppController($http) {
     var vm = this;
+    vm.newPoke = {};
+
     $http({
         method: 'GET',
         url: 'https://super-crud.herokuapp.com/pokemon'
     }).then(function successCallback(response) {
-      // console.log(response);
+        // console.log(vm.data);
+        // console.log(response);
         vm.pokemon = response.data.pokemon;
         console.log(response.data.pokemon);
-        // console.log(vm.pokemon);
     }, function errorCallback(response) {
         console.log('There was an error getting the data', response);
     });
+
+    vm.createPoke = function() {
+        console.log("CREATE");
+        $http({
+            method: 'POST',
+            url: 'https://super-crud.herokuapp.com/pokemon',
+            data: vm.newPoke,
+        }).then(function successCallback(response) {
+            vm.pokemon.push(response.data);
+        }, function errorCallback(response) {
+            console.log('There was an error posting the data', response);
+        });
+    };
+
+
     vm.deletePoke = function(poke) { //come back to this later, page doesn't render SAP
         $http({
             method: 'DELETE',
             url: 'https://super-crud.herokuapp.com/pokemon/' + poke
         }).then(function successCallback(json) {
-          console.log(json);
+            console.log(json);
+            var index = vm.pokemon.indexOf(poke);
+            vm.pokemon.splice(index, 1);
         }, function errorCallback(response) {
-          console.log(poke);
+            console.log(poke);
             console.log('There was an error deleting the data', response);
         });
     };
 
     vm.editPoke = function(poke) {
-      $http({
-        method: 'PUT',
-        url: 'https://super-crud.herokuapp.com/pokemon/' + poke
-      }).then(function successCallback(json) {
-        console.log(json);
-      }, function errorCallback(response) {
-        console.log('There was an error deleting the data', response);
-      });
+        console.log(poke);
+        $http({
+            method: 'PUT',
+            url: 'https://super-crud.herokuapp.com/pokemon/' + poke._id,
+            data: poke
+        }).then(function successCallback(json) {
+            console.log(json);
+        }, function errorCallback(response) {
+            console.log('There was an error deleting the data', response);
+        });
     };
-//     vm.createAlbum = function() {
-//         $http({
-//             method: 'POST',
-//             url: '/api/albums',
-//             data: vm.newAlbum,
-//         }).then(function successCallback(response) {
-//             vm.albums.push(response.data);
-//         }, function errorCallback(response) {
-//             console.log('There was an error posting the data', response);
-//         });
-//     };
-//
-//     };
 }
